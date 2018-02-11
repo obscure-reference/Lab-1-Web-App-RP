8200fd56fb5878b2766914f5e84aa68c84e8e0c9
Roman Polonyankin <Roman200333@hotmail.ca>
2018-02-10 22:09:25 -0500

    Add project files.

---
+++ Lab 1 Web Application/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_1_Web_Application.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Products()
+        {
+         return View();
+        }
+
+
+
+
+        public ActionResult ProductReturned(string prod)
+        {
+
+         ViewBag.Prod = prod;
+         return View();
+
+        }
+
+    }
+}
+
---
+++ Lab 1 Web Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab_1_Web_Application.Startup))]
+namespace Lab_1_Web_Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
